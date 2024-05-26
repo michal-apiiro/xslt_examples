b8a28d5467882cb7f84a6964a5b05cc933ee61a5
Michal Feldman <michal@apiiro.com>
2024-05-26 17:10:39 +0300

    added python flask API files

---
+++ Python/restful.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class TodoSimple(Resource):
+    def get(self):
+        return 'Hi'
+    
+    def post(self):
+        return 'Hi'
+
+class Index(Resource):
+    def get(self):
+        return 'Hi'
+    
+    def post(self):
+        return 'Hi'
+    
+api.add_resource(TodoSimple, '/todos/<string:todo_id>')
+api.add_resource(Index, '/index/<string:index_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
