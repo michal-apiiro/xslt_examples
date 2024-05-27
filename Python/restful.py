e9d4269f944c8955cb2528458b8cd490b680e433
Michal Feldman <michal@apiiro.com>
2024-05-27 20:14:16 +0300

    flask restful

--- Python/restful.py
+++ Python/restful.py
@@ -1,11 +1,10 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, req
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-
-class TodoSimple(Resource):
+class Hello(Resource):
     def get(self):
         return 'Hi'
     
@@ -19,7 +18,7 @@ class Index(Resource):
     def post(self):
         return 'Hi'
     
-api.add_resource(TodoSimple, '/todos/<string:todo_id>')
+api.add_resource(Hello, '/hello/<string:todo_id>')
 api.add_resource(Index, '/index/<string:index_id>')
 
 if __name__ == '__main__':
